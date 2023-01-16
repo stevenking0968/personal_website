3d710c25f7494c7e03648eeef4dbc5fd4c4bb663
mymacbook <mymacbook@mymacbooks-MacBook-Pro.local>
2023-01-16 14:25:30 -0800

    update to frontend only app

--- client/src/Resume.js
+++ client/src/Resume.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PDFViewer from './PDFViewer'
+import Resume from "./resume.pdf";
 
 export default function Patents() {
 	  return (
 	    <div className="patents" style={{marginTop:'30px'}}>
 	      <h4>Resume</h4>
-	      <PDFViewer fileUrl={"/resume_file"} />
+	      <PDFViewer fileUrl={Resume} />
 	    </div>
 	  );
 }
--- client/src/Slides.js
+++ client/src/Slides.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PDFViewer from './PDFViewer'
 import LinkedIn from './linkedin.png'
-
+import Presentation from './presentation.pdf'
 export default function Slides() {
 
   return (
@@ -9,7 +9,7 @@ export default function Slides() {
 	  <div style={{marginTop:'30px'}}>
       <h4>Slides</h4>
       <p>I am a mechanical engineer with 25+ years of experience. Check out my background and projects below.</p>
-      <PDFViewer fileUrl={"/presentation"} />
+      <PDFViewer fileUrl={Presentation} />
       </div>
     </div>
   );
